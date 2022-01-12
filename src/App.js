14f154c761ef70006647efafdc7aa498d2ea7faf
kimmohakkarainen <30873934+kimmohakkarainen@users.noreply.github.com>
2022-01-12 19:22:35 +0200

    sidebar finetuned

--- src/App.js
+++ src/App.js
@@ -3,43 +3,15 @@ import ReactDOM from "react-dom";
 
 import { Navbar, Container, Nav, Row } from "react-bootstrap";
 
+import { Menubar, Sidebar } from "./mainview";
+
 export default function App() {
   return (
     <>
-      <Navbar sticky="top" bg="light" expand="md">
-        <Container fluid>
-          <Navbar.Brand className="col-md-3 col-md-2 d-md-block">
-            PL2
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link>Daily Dashboard</Nav.Link>
-              <Nav.Link>Managemement</Nav.Link>
-              <Nav.Link>Statistics</Nav.Link>
-              <Nav.Link>Notifications</Nav.Link>
-              <Nav.Link>Work</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <Menubar />
       <Container fluid>
         <Row>
-          <Nav
-            bg="light"
-            expand="md"
-            className="col-md-3 col-lg-2 col-md-block sidebar"
-          >
-            <div style={{ position: "sticky", paddingTop: "1rem" }}>
-              <Nav.Link>Employee</Nav.Link>
-              <Nav.Link>Wellbeing</Nav.Link>
-              <Nav.Link>Absence</Nav.Link>
-              <Nav.Link>Recruitment</Nav.Link>
-              <Nav.Link>Development</Nav.Link>
-              <Nav.Link>Induction</Nav.Link>
-              <Nav.Link>Termination</Nav.Link>
-            </div>
-          </Nav>
+          <Sidebar />
         </Row>
       </Container>
     </>
---
+++ src/mainview/index.js
@@ -0,0 +1,201 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { Navbar, Container, Nav, Row } from "react-bootstrap";
+
+export function Menubar() {
+  return (
+    <Navbar
+      sticky="top"
+      bg="dark"
+      variant="dark"
+      expand="md"
+      className="shadow"
+    >
+      <Container fluid>
+        <Navbar.Brand className="col-md-3 col-md-2 d-md-block">
+          PL2
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link>Daily Dashboard</Nav.Link>
+            <Nav.Link>Managemement</Nav.Link>
+            <Nav.Link>Statistics</Nav.Link>
+            <Nav.Link>Notifications</Nav.Link>
+            <Nav.Link>Work</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export function Sidebar() {
+  return (
+    <Nav
+      bg="light"
+      expand="md"
+      className="col-md-3 col-lg-2 col-md-block sidebar"
+    >
+      <div
+        style={{
+          position: "sticky",
+          paddingTop: "1rem"
+        }}
+      >
+        <Nav.Link>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-users"
+            aria-hidden="true"
+            style={{ verticalAlign: "text-bottom", marginRight: "4px" }}
+          >
+            <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+            <circle cx="9" cy="7" r="4"></circle>
+            <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
+            <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
+          </svg>
+          Employee
+        </Nav.Link>
+        <Nav.Link>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-shopping-cart"
+            aria-hidden="true"
+            style={{ verticalAlign: "text-bottom", marginRight: "4px" }}
+          >
+            <circle cx="9" cy="21" r="1"></circle>
+            <circle cx="20" cy="21" r="1"></circle>
+            <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+          </svg>
+          Wellbeing
+        </Nav.Link>
+        <Nav.Link>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-bar-chart-2"
+            aria-hidden="true"
+            style={{ verticalAlign: "text-bottom", marginRight: "4px" }}
+          >
+            <line x1="18" y1="20" x2="18" y2="10"></line>
+            <line x1="12" y1="20" x2="12" y2="4"></line>
+            <line x1="6" y1="20" x2="6" y2="14"></line>
+          </svg>
+          Absence
+        </Nav.Link>
+        <Nav.Link>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-file"
+            aria-hidden="true"
+            style={{ verticalAlign: "text-bottom", marginRight: "4px" }}
+          >
+            <path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path>
+            <polyline points="13 2 13 9 20 9"></polyline>
+          </svg>
+          Recruitment
+        </Nav.Link>
+        <Nav.Link>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-layers"
+            aria-hidden="true"
+            style={{ verticalAlign: "text-bottom", marginRight: "4px" }}
+          >
+            <polygon points="12 2 2 7 12 12 22 7 12 2"></polygon>
+            <polyline points="2 17 12 22 22 17"></polyline>
+            <polyline points="2 12 12 17 22 12"></polyline>
+          </svg>
+          Development
+        </Nav.Link>
+        <Nav.Link>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-file-text"
+            aria-hidden="true"
+            style={{ verticalAlign: "text-bottom", marginRight: "4px" }}
+          >
+            <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+            <polyline points="14 2 14 8 20 8"></polyline>
+            <line x1="16" y1="13" x2="8" y2="13"></line>
+            <line x1="16" y1="17" x2="8" y2="17"></line>
+            <polyline points="10 9 9 9 8 9"></polyline>
+          </svg>
+          Induction
+        </Nav.Link>
+        <Nav.Link>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-file-text"
+            aria-hidden="true"
+            style={{ verticalAlign: "text-bottom", marginRight: "4px" }}
+          >
+            <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+            <polyline points="14 2 14 8 20 8"></polyline>
+            <line x1="16" y1="13" x2="8" y2="13"></line>
+            <line x1="16" y1="17" x2="8" y2="17"></line>
+            <polyline points="10 9 9 9 8 9"></polyline>
+          </svg>
+          Termination
+        </Nav.Link>
+      </div>
+    </Nav>
+  );
+}
